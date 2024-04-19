696e54cef9fe312375069f1a4ca11e5aced645cb
Arjitjain16 <arjitvansh16@gmail.com>
2024-04-19 23:43:08 +0530

    controllers commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+import userRoutes from "./routes/user.route.js"
+
 import connectToMongoDb from "./db/ConnecttoMongoDb.js"
 
 const app = express()
@@ -13,9 +17,14 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json()) // middleware for req.body
 app.use(express.urlencoded({extended: true})) // middleware for req.body
+app.use(cookieParser())
 
-
+// route ko use krna hai 
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+
 app.get('/',function(req,res){
     res.send("Hello")
 })
